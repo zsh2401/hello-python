11cdf2a1c90083a02092c26ebdb5f951998162c5
zsh2401 <zsh2401@163.com>
2020-08-02 22:29:37 +0800

    3.2 has been finished

---
+++ day01/hw/lhl/3-1.py
@@ -0,0 +1,8 @@
+name = ['lhl','yb','zsh']
+print(name[1])#3-1
+print(name[2]+"永远滴神")#3-2
+'''交通工具
+lhl想要一辆自行车，但是我只有一辆汽车
+'''
+method = ['car','bike','bus']
+print(name[0]+" "+"want a"+" "+method[1]+", "+"but I only have a"+method[0])#3-3
