6082071335123251cc653cf5667bce02031bb91f
zsh2401 <zsh2401@163.com>
2020-08-06 22:49:29 +0800

    Adding some files.

---
+++ day00/P.java
@@ -0,0 +1,10 @@
+public class P{
+    public static void main(String... args){
+
+        int a = 0;
+        float 
+        double
+
+
+    }
+}
---
+++ day00/var.py
@@ -0,0 +1,6 @@
+a = 0
+b = "Hello Var"
+c = 0.0
+print(a);
+print(b);
+print(c);
